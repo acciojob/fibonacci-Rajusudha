118a3d6b5555aeb0c41970444ce80fe7d2272ffc
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-09 19:54:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-function fibonacci(num) {
-// your code here
-	let a=0;
-	let b=1;
-	let i=1;
-	if(num==1){
-		consolge.log("0");
-		else if(num==2 && num==3){
-		consolge.log("1");
-		}
-	}
-	while(i<=num){
-		let sum=a+b;
-		a=b;
-		b=sum;
-		i++;
-		
-	}
-consolge.log(fibonacci);
-	
-}
+def fibonacci(num):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, num):
+            a, b = b, a + b
+        return b
 
-module.exports = fibonacci;
+# Test cases
+print(fibonacci(1))  # Output: 0
+print(fibonacci(5))  # Output: 3
+
