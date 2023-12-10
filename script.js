9cd2d65384f0b03dd66fb4758a0c649dc93f3c7a
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-10 14:38:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 def fibonacci(num):
-    if num == 0:
-        return 0
+    if num <= 0:
+        return "Input must be a positive integer"
     elif num == 1:
+        return 0
+    elif num == 2:
         return 1
     else:
         a, b = 0, 1
-        for _ in range(2, num):
+        for _ in range(num - 2):
             a, b = b, a + b
         return b
 
-# Test cases
+# Examples
 print(fibonacci(1))  # Output: 0
 print(fibonacci(5))  # Output: 3
+
 
