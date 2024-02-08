03fe30bca803c09714f64dd63723efe9c419dcd6
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-02-08 14:30:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-def fibonacci(num):
-    if num <= 0:
-        return "Input must be a positive integer"
-    elif num == 1:
-        return 0
-    elif num == 2:
-        return 1
-    else:
-        a, b = 0, 1
-        for _ in range(num - 2):
-            a, b = b, a + b
-        return b
+function fibonacci(num) {
+    if (num <= 0) {
+        return "Invalid input. Please provide a positive integer greater than 0.";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        let a = 0, b = 1, temp;
+        for (let i = 3; i <= num; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
 
-# Examples
-print(fibonacci(1))  # Output: 0
-print(fibonacci(5))  # Output: 3
+// Examples
+console.log(fibonacci(1)); // Output: 0
+console.log(fibonacci(5)); // Output: 3
 
 
